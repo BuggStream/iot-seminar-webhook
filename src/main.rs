73a498505ad0a3b9e2c013700b76274bdff1980c
BuggStream <buggstream@tuta.io>
2024-10-30 10:40:27 +0100

    Update webhook endpoints

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use axum::{
     http::StatusCode,
     Json, Router,
 };
-use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
@@ -12,46 +13,44 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
-        // `POST /users` goes to `create_user`
-        // .route("/users", post(create_user));
+        .route("/", get(root))
+        .route("/uplink", post(uplink))
+        .route("/join", post(join))
+        .route("/location", post(location));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    info!("Listening on http://0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
+    info!("Received message at root");
     "Hello, World!"
 }
 
-// async fn create_user(
-//     // this argument tells axum to parse the request body
-//     // as JSON into a `CreateUser` type
-//     Json(payload): Json<CreateUser>,
-// ) -> (StatusCode, Json<User>) {
-//     // insert your application logic here
-//     let user = User {
-//         id: 1337,
-//         username: payload.username,
-//     };
-//
-//     // this will be converted into a JSON response
-//     // with a status code of `201 Created`
-//     (StatusCode::CREATED, Json(user))
-// }
-//
-// // the input to our `create_user` handler
-// #[derive(Deserialize)]
-// struct CreateUser {
-//     username: String,
-// }
-//
-// // the output to our `create_user` handler
-// #[derive(Serialize)]
-// struct User {
-//     id: u64,
-//     username: String,
-// }
+async fn uplink(
+    Json(payload): Json<Value>,
+) -> StatusCode {
+    info!("Uplink json:\n{}", payload.to_string());
+
+    StatusCode::OK
+}
+
+async fn join(
+    Json(payload): Json<Value>,
+) -> StatusCode {
+    info!("Join json:\n{}", payload.to_string());
+
+    StatusCode::OK
+}
+
+async fn location(
+    Json(payload): Json<Value>,
+) -> StatusCode {
+    info!("Location json:\n{}", payload.to_string());
+
+    StatusCode::OK
+}
+
