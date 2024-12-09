41e51a4dfed4b6fae48a78f5b88c56a5173d9434
Jim van Vliet <j.i.m.vanvliet@student.tudelft.nl>
2024-12-09 17:36:48 +0100

    Add simple setup for adding data to database

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,73 @@
-use std::env;
+use axum::extract::State;
 use axum::{
-    routing::{get, post},
     http::StatusCode,
+    routing::{get, post},
     Json, Router,
 };
+use dotenv::dotenv;
 use serde::Deserialize;
 use serde_json::Value;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::env;
+use std::time::Duration;
 use tokio::net::TcpListener;
 use tracing::info;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     // initialize tracing
     tracing_subscriber::fmt::init();
 
     let port = env::var("PORT").unwrap_or("3000".to_string());
+
+    let db_url = env::var("DATABASE_URL").expect("No database URL variable configured!");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_url)
+        .await
+        .expect("can't connect to database");
 
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
         .route("/uplink", post(uplink))
         .route("/join", post(join))
-        .route("/location", post(location));
+        .route("/location", post(location))
+        .with_state(pool);
 
     let url = format!("0.0.0.0:{}", port);
 
     // run our app with hyper, listening globally on port 3000
     let listener = TcpListener::bind(&url).await.unwrap();
-    info!("Listening on http://{}", &url);
+    info!("Listening on {}", &url);
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
+async fn root(State(pool): State<PgPool>) -> Result<&'static str, (StatusCode, String)> {
     info!("Received message at root");
-    if let Ok(url) = env::var("DATABASE_URL") {
-        info!("Database URL: {}", url);
-    } else {
-        info!("No database URL variable set");
-    }
 
-    "Hello, World!"
+    sqlx::query("INSERT INTO public.uplink(message) VALUES ($1)")
+        .bind(sqlx::types::Json("{}"))
+        .execute(&pool)
+        .await
+        .map_err(database_error)?;
+
+    let results = sqlx::query("SELECT * FROM uplink")
+        .fetch_all(&pool)
+        .await
+        .map_err(database_error)?;
+
+    info!("Results: {}", results.len());
+
+    Ok("Hello, World!")
 }
 
-async fn uplink(
-    Json(payload): Json<Uplink>,
-) -> StatusCode {
+async fn uplink(Json(payload): Json<Uplink>) -> StatusCode {
     let decoded = &payload.uplink_message.decoded_payload;
     info!("Decoded payload json:\n{}", decoded);
     info!("Entire payload json:\n{:?}", payload);
@@ -53,17 +75,13 @@ async fn uplink(
     StatusCode::OK
 }
 
-async fn join(
-    Json(payload): Json<Value>,
-) -> StatusCode {
+async fn join(Json(payload): Json<Value>) -> StatusCode {
     info!("Join json:\n{}", payload.to_string());
 
     StatusCode::OK
 }
 
-async fn location(
-    Json(payload): Json<Value>,
-) -> StatusCode {
+async fn location(Json(payload): Json<Value>) -> StatusCode {
     info!("Location json:\n{}", payload.to_string());
 
     StatusCode::OK
@@ -81,4 +99,9 @@ struct UplinkMessage {
     rx_metadata: Value,
 }
 
-
+fn database_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
